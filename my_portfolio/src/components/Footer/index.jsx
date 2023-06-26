77bb26d16f4e515cd8966520d3b31fcc48348bf7
Toba-dean <tobaogundimu@gmail.com>
2023-06-26 16:09:03 +0100

    added the footer component, but haing issues with hydration

--- my_portfolio/src/components/Footer/index.jsx
+++ my_portfolio/src/components/Footer/index.jsx
@@ -1,7 +1,71 @@
-import './footer.styles.css';
+'use client';
+
+import { useEffect, useState } from 'react';
+import { AiFillGithub, AiOutlineTwitter, AiFillLinkedin } from 'react-icons/ai';
+
+import './footer.styles.scss';
 
 export default function Footer() {
+
+  const time = new Date().toLocaleTimeString('en-US', {
+    hour12: false
+  });
+  const [currentTime, setCurrentTime] = useState(time)
+  const footerData = [
+    {
+      id: 1,
+      icon: <AiFillGithub />,
+      path: 'https://github.com/Toba-dean'
+    },
+    {
+      id: 2,
+      icon: <AiOutlineTwitter />,
+      path: 'https://twitter.com/code_DE4N'
+    },
+    {
+      id: 2,
+      icon: <AiFillLinkedin />,
+      path: '/',
+      last: 'last'
+    }
+  ];
+
+  useEffect(() => {
+    const updateTime = () => {
+      const time = new Date().toLocaleTimeString('en-US', {
+        hour12: false
+      });
+      setCurrentTime(time)
+    }
+
+    setInterval(updateTime, 1000);
+
+    return () => clearInterval(updateTime);
+  }, []);
+
   return (
-    <div>Footer</div>
+    <footer>
+      <p className="logo">
+        find me at:
+      </p>
+
+      {
+        footerData.map(({ id, icon, path, last }) => (
+          <a
+            href={path}
+            key={id}
+            target="_blank"
+            rel="noreferrer"
+            className={last && last}
+          >
+            {icon}
+          </a>
+        ))
+      }
+
+      <div className="left">
+        {currentTime}
+      </div>
+    </footer>
   )
 }
