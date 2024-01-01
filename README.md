7df609d870f0ecbc5a097456789100fbf2ace1e2
Toba-dean <tobaogundimu@gmail.com>
2024-01-01 17:44:27 +0100

    updates

---
+++ README.md
@@ -0,0 +1,16 @@
+By toba
+
+# Portfolio App
+
+![Portfolio App](https://i.ibb.co/VvZ4JCW/port.png)
+
+## 📣 Summary
+My portfolio website build with Next JS. It is a multi page app having the home, about project and contact pages respectively. I used sanity as the content management system (CMS) form storing and retrieving my project data and also used node mailer for male management to enable free email sending from the app to my email address, and used sass for styling.
+
+In this project i made use of the following:
+
+- Next Js
+- @sanity/client
+- @sanity/image-url
+- nodemailer
+- sass
--- my_portfolio/src/app/page.js
+++ my_portfolio/src/app/page.js
@@ -1,13 +1,13 @@
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
 
 // const Game = dynamic(() => import('@/container/game'), { ssr: false });
-import { Game } from '@/container';
-import '@/styles/main.styles.scss';
+import { Game } from "@/container";
+import "@/styles/main.styles.scss";
 
 export default function Home() {
   return (
-    <div className='main'>
-      <div className='left'>
+    <div className="main">
+      <div className="left">
         <div className="top">
           <p className="intro">👋 ¡Hola!, Je suis</p>
           <h1>Sheriff Dean</h1>
@@ -19,12 +19,8 @@ export default function Home() {
         </div>
 
         <div className="bottom">
-          <p>
-            {"// a self taught web developer."}
-          </p>
-          <p>
-            {"// surf around to know more about me and what i do."}
-          </p>
+          <p>{"// a self taught web developer."}</p>
+          <p>{"// surf around to know more about me and what i do."}</p>
 
           <p className="resume">
             <a href="/">Resume</a>
@@ -32,9 +28,9 @@ export default function Home() {
         </div>
       </div>
 
-      <div className='right'>
+      <div className="right">
         <Game />
       </div>
     </div>
-  )
+  );
 }
